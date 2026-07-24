6ae1f2402bd586a1c2b36691407179cf9c0dca60
yuvan8888 <yuvanrajav@gmail.com>
2026-07-24 17:47:10 +0530

    new file created

---
+++ Desktop/Job Portal/backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Job Portal Backend");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
